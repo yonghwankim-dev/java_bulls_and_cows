72d825899bd6ec8d3c1d3286e1c9c72b8d54060f
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-10-18 22:11:28 +0900

    [추가] 테스트 코드 추가

--- src/main/java/kr/yh/Game.java
+++ src/main/java/kr/yh/Game.java
@@ -1,15 +1,75 @@
 package kr.yh;
 
-public class Game {
-    public int numberOfBall;
-    public int[] rightAnswers;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 
+public class Game {
+    private int numberOfBall;
+    private int currentRound;
+    private int[] rightAnswers;
+    private Set<Integer> rightAnswerSet;
     public Game(int numberOfBall) {
         this.numberOfBall = numberOfBall;
+        this.currentRound = 1;
         this.rightAnswers = new int[numberOfBall];
+        this.rightAnswerSet = new HashSet<>();
     }
 
-    public int[] createRightAnswers(){
-        return new int[]{0,0,0};
+    public void start(){
+        initGame();
+        while(currentRound <= 9){
+            displayCurrentRound();
+            int[] numbersOfClient = inputNumbersFromClient();
+            passRound();
+        }
     }
+
+    private void initGame(){
+        createRightAnswers();
+        resetCurrentRound();
+    }
+
+    private void createRightAnswers(){
+        int idx = 0;
+        rightAnswerSet.clear();
+        while(rightAnswerSet.size() < 3){
+            int randomNumber = createRandomNumber();
+            if(!rightAnswerSet.contains(randomNumber)){
+                rightAnswerSet.add(randomNumber);
+                rightAnswers[idx++] = randomNumber;
+            }
+        }
+    }
+
+    private int createRandomNumber(){
+        return new Random().nextInt(10);
+    }
+
+    private void resetCurrentRound(){
+        currentRound = 1;
+    }
+
+    private void passRound(){
+        currentRound++;
+    }
+
+    private void displayCurrentRound(){
+        System.out.printf("현재 라운드 : %d\n", currentRound);
+    }
+
+    private int[] inputNumbersFromClient() {
+        int[] result;
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+            result = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+
 }
